b4b8a1cbc6b717f1274f8e7a152499092ae34b79
Seereen Yangpaknam <seereen_2540@gmail.com>
2025-07-07 23:44:13 +0700

    feat:add routes data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -19,6 +19,8 @@ import { HooksPage } from "../pages/HooksPage";
 import { UseEffectWithInterval } from "../components/3.hooks/2.UseEffectWithInterval/UseEffectWithInterval";
 import { UseEffectWithDependency } from "../components/3.hooks/3.UseEffectWithDependency/UseEffectWithDependency";
 import { UseEffectConditional } from "../components/3.hooks/4. UseEffectConditional/UseEffectConditional";
+import { UseMemoBasic } from "../components/3.hooks/5.UseMemo/UseMemoBasic";
+import { UseCallbackBasic } from "../components/3.hooks/6.UseCallback/UseCallbackBasic";
 
 // หัวข้อต่างๆ
 export const sectionRoutes = [
@@ -117,5 +119,15 @@ export const hooksRoutes = [
     path: "useEffect-conditional",
     name: "4.UseEffect Conditional",
     element: <UseEffectConditional />,
+  },
+  {
+    path: "useMemo-basic",
+    name: "5.UseMemo Basic",
+    element: <UseMemoBasic />,
+  },
+  {
+    path: "useCallback-basic",
+    name: "6.UseCallback Basic",
+    element: <UseCallbackBasic />,
   },
 ];
